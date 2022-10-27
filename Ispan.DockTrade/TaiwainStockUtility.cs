55b28dd7a651264387f787a46ef1e6f5a77672b5
Jasper <guitar1859@gmail.com>
2022-10-27 19:17:42 +0800

    加入專案檔案。

---
+++ Ispan.DockTrade/TaiwainStockUtility.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispan.DockTrade
+{
+    public class program
+    {
+        
+    }
+    public class TaiwainStockUtility
+    {
+        /// <summary>
+        /// 
+
+        /// </summary>
+        /// 日期必需在星期一~星期五時間必需介於9:00 ~ 13:30
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public bool IsTradingTime(DateTime dt)
+        {
+            bool isDateTime = DateTime.TryParse(dt.ToString(), out dt);
+            if (isDateTime == false)
+            {
+                throw new Exception("您輸入的日期不符合格式");
+            }
+            
+            string dtString = dt.ToString("HH:mm");
+            DateTime dateTime = Convert.ToDateTime(dtString);
+            DateTime start = Convert.ToDateTime("09:00");
+            DateTime end = Convert.ToDateTime("13:30");
+
+            int week = (int)dt.DayOfWeek;
+            
+            if (week > 0 && week <= 5 && dateTime >= start && dateTime <= end)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        
+
+    }
+}
---
+++ Ispan.StockTradeTest/TaiwainStockUtilityTest.cs
@@ -0,0 +1,36 @@
+﻿using Ispan.DockTrade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispan.StockTradeTest
+{
+    internal class TaiwainStockUtilityTest
+    {
+        [TestCase("2022,10,21 10:00", true)]
+        [TestCase("2022,10,22 10:00", false)]
+        [TestCase("2022,10,23 10:00", false)]
+        [TestCase("2022,10,24 10:00", true)]
+        public void IsTradingTime_周末是否為false(DateTime dt, bool actual )
+        {
+            var obj = new TaiwainStockUtility();
+            var isTradingTime = obj.IsTradingTime(dt);
+            Assert.AreEqual(actual, isTradingTime);
+
+        }
+
+        [TestCase("2022,10,24 8:59", false)]
+        [TestCase("2022,10,24 9:00", true)]
+        [TestCase("2022,10,24 13:30", true )]
+        [TestCase("2022,10,24 13:31", false)]
+        public void IsTradingTime_時間是否在時間內為true(DateTime dt, bool actual)
+        {
+            var obj = new TaiwainStockUtility();
+            var isTradingTime = obj.IsTradingTime(dt);
+            Assert.AreEqual(actual, isTradingTime);
+
+        }
+    }
+}
